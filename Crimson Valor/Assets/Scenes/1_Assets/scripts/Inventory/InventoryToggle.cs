e074dd41f338275e20237f0911558a41b7467a8a
PhoenixRider <rg0102@srmist.edu.in>
2026-01-10 19:04:34 +0530

    OpenWorld VR
    map design update
    inventory system

---
+++ Crimson Valor/Assets/Scenes/1_Assets/scripts/Inventory/InventoryToggle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InventoryToggle : MonoBehaviour
+{
+    [SerializeField] private GameObject inventoryVisual;
+    [SerializeField] private InventoryStorage storage;
+    [SerializeField] private InputActionReference toggleAction;
+
+    private void OnEnable()
+    {
+        toggleAction.action.Enable();
+        toggleAction.action.performed += Toggle;
+    }
+
+    private void OnDisable()
+    {
+        toggleAction.action.performed -= Toggle;
+        toggleAction.action.Disable();
+    }
+
+    private void Toggle(InputAction.CallbackContext ctx)
+    {
+        bool open = !inventoryVisual.activeSelf;
+
+        if (!open)
+            storage.StoreAll();
+        else
+            storage.RestoreAll();
+
+        inventoryVisual.SetActive(open);
+    }
+}
